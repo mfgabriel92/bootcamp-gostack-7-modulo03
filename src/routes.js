386e30c98263fe5b5ef50ef1a5130a5350feec6b
Gabriel Monteiro Fernandes <mfgbriel92@gmail.com>
2019-06-24 20:44:26 -0300

    Módulo 02 - Nodemon & Sucrase

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,9 @@
-const { Router } = require('express')
+import { Router } from 'express'
+
 const routes = new Router()
 
 routes.get('/', (req, res) => {
   res.json({ message: 'Hello world' })
 })
 
-module.exports = routes
+export default routes
--- src/server.js
+++ src/server.js
@@ -1,3 +1,3 @@
-const app = require('./app')
+import app from './app'
 
 app.listen(3333, () => console.log('The server is running on port 3333'))
