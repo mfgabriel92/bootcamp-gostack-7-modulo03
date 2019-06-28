9d5a4253559873d3ccae6c6a58ec8d22d08955bd
Gabriel Monteiro Fernandes <mfgbriel92@gmail.com>
2019-06-28 14:38:23 -0300

    Módulo 03 - Tratamento de exceções

--- src/app.js
+++ src/app.js
@@ -1,17 +1,26 @@
 import express from 'express'
 import { resolve } from 'path'
+import * as Sentry from '@sentry/node'
+import Youch from 'youch'
+import 'express-async-errors'
 import routes from './routes'
+import sentryConfig from './config/sentry'
+import HTTP from './utils/httpResponse'
 import './database'
 
 class App {
   constructor() {
     this.server = express()
 
+    Sentry.init(sentryConfig)
+
     this.middlewares()
     this.routes()
+    this.exceptions()
   }
 
   middlewares() {
+    this.server.use(Sentry.Handlers.requestHandler())
     this.server.use(express.json())
     this.server.use(
       '/files',
@@ -21,6 +30,14 @@ class App {
 
   routes() {
     this.server.use(routes)
+    this.server.use(Sentry.Handlers.errorHandler())
+  }
+
+  exceptions() {
+    this.server.use(async (err, req, res, next) => {
+      const errors = await new Youch(err, req).toJSON()
+      return res.status(HTTP.INTERNAL_SERVER_ERROR).json({ errors })
+    })
   }
 }
 
---
+++ src/config/sentry.js
@@ -0,0 +1,3 @@
+export default {
+  dsn: 'https://81b3c9f52ace41c3b64dc00d0a5b76de@sentry.io/1492859',
+}
